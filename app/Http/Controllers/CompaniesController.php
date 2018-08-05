7337fdd57b2e27826865c91ab3d56317012e4e8b
thiagometal <thiagometal.fne@gmail.com>
2018-08-05 17:00:20 -0400

    fix bug in import models

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class CompaniesController extends Controller
 {
