2d9a08c675c0cf960c5a15e3ba7006ad574492dc
thiagometal <thiagometal.fne@gmail.com>
2018-08-05 16:24:53 -0400

    add routes

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(array('prefix' => 'api'), function()
+{
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  Route::get('/', function () {
+      return response()->json(['message' => 'Jobs API', 'status' => 'Connected']);;
+  });
+
+  Route::resource('jobs', 'JobsController');
+  Route::resource('companies', 'CompaniesController');
+});
+
+Route::get('/', function () {
+    return redirect('api');
 });
