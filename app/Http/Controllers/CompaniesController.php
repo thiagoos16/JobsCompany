1e8f3c7c2d693382ed69a4c2c0cafeb463c5fe88
thiagometal <thiagometal.fne@gmail.com>
2018-08-05 17:04:37 -0400

    add update company and jobs method

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -37,4 +37,20 @@ class CompaniesController extends Controller
 
         return response()->json($company, 201);
     }
+
+    public function update(Request $request, $id)
+    {
+        $company = Company::find($id);
+
+        if(!$company) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $company->fill($request->all());
+        $company->save();
+
+        return response()->json($company);
+    }
 }
--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -37,4 +37,20 @@ class JobsController extends Controller
 
         return response()->json($job, 201);
     }
+
+    public function update(Request $request, $id)
+    {
+        $job = Job::find($id);
+
+        if(!$job) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $job->fill($request->all());
+        $job->save();
+
+        return response()->json($job);
+    }
 }
