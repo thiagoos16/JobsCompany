ae56fdb57dfb2e70894c15ab72de636ff4453f67
thiagometal <thiagometal.fne@gmail.com>
2018-08-05 17:05:40 -0400

    add destroy company and jobs methods

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -53,4 +53,17 @@ class JobsController extends Controller
 
         return response()->json($job);
     }
+
+    public function destroy($id)
+    {
+        $job = Job::find($id);
+
+        if(!$job) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $job->delete();
+    }
 }
