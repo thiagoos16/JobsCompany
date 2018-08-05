3fbb110afbd6dac9288735f30390afaa14c08524
thiagometal <thiagometal.fne@gmail.com>
2018-08-05 17:21:32 -0400

    add message in unauthenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,16 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
+        // if (Auth::guard($guard)->check()) {
+        //     return redirect('/home');
+        // }
 
-        return $next($request);
+        // return $next($request);
+
+        if ($this->auth->guest()) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
     }
 }
