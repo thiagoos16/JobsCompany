50bc091c35d9c5cda9ef1f4478406936bee679c3
thiagometal <thiagometal.fne@gmail.com>
2018-08-05 16:20:59 -0400

    add models specifications

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = ['name', 'email', 'website', 'logo', 'password'];
+
+    protected $hidden = ['password'];
+
+    protected $dates = ['deleted_at'];
+
+    public function jobs()
+    {
+        return $this->hasMany('App\Job');
+    }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'local', 'remote', 'type', 'company_id'];
+
+    protected $dates = ['deleted_at'];
+
+    function company() {
+        return $this->belongsTo('App\Company');
+    }
 }
